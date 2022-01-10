2f24ad467a3bc3a4b7c826616c5eea6d5db34671
Devkid3000 <chizaramprovidence2050@gmail.com>
2022-01-10 03:23:46 +0100

    first commit

---
+++ errors.js
@@ -0,0 +1,7 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    setTimeout(function() {
+        window.location = 'shoplife.html'
+    }, 0500);
+})
